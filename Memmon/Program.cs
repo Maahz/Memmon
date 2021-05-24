455936a48ed18f36f25b1c62a7e4e3fb38042676
Markku Sjöblom <markku.sjoblom@gmail.com>
2021-05-24 09:11:27 +0300

    Add project files.

---
+++ Memmon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.Devices;
+
+
+namespace Memmon
+{
+    class Program
+    {
+        internal const ulong kLimit = 22684354560;  //Warning limit
+
+        internal static ComputerInfo ci = new ComputerInfo();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                //If memory is lower than the limit, show warning
+                if (MemoryLow()) 
+                    MessageBox.Show("Memory running low!!!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                System.Threading.Thread.Sleep(10000);
+            }
+        }
+
+        private static bool MemoryLow()
+        {
+            if (ci.AvailablePhysicalMemory < kLimit) return true;
+            return false;
+        }
+    }
+}
